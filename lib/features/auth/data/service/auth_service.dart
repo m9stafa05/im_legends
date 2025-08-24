bcd98d49307113beb1b61339703f3aaa2ddde5f2
Mustafa Elbaz <m9stafa05@gmail.com>
2025-08-24 19:54:16 +0300

    Create Firebase Auth call to login And Sign Up

---
+++ lib/features/auth/data/service/auth_service.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> login({required String email, required String password}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      // Re-throw error to be handled in Cubit
+      if (e.code == 'user-not-found') {
+        throw Exception('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        throw Exception('Wrong password provided.');
+      } else {
+        throw Exception(e.message ?? 'Login failed.');
+      }
+    }
+  }
+
+  Future<void> signUp({required String email, required String password}) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw Exception('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw Exception('The account already exists for that email.');
+      } else {
+        throw Exception(e.message ?? 'Sign-up failed.');
+      }
+    }
+  }
+}
