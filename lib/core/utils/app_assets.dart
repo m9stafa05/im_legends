0a9a29bd962018b137eaf40ade9e1f7f84edebc7
Mustafa Elbaz <m9stafa05@gmail.com>
2025-08-25 15:45:38 +0300

    Fix Font family Assets

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -2,22 +2,10 @@ class AppAssets {
   //Svgs 
   static const String appLogo = 'assets/svgs/logo.svg';
 
-  // Fonts
-
-    // # Tajawal
-  // static const String fontTajawalRegular = 'assets/fonts/Tajawal/Tajawal-Regular.ttf';
-  // static const String fontTajawalBold = 'assets/fonts/Tajawal/Tajawal-bold.ttf';
-  // static const String fontTajawalMedium = 'assets/fonts/Tajawal/Tajawal-Medium.ttf';
-  // static const String fontTajawalSemiBold = 'assets/fonts/Tajawal/Tajawal-SemiBold.ttf';
-
-    // # Federant
-  static const String fontFederantRegular = 'assets/fonts/Federant/Federant-Regular.ttf';
-
-    // # BebasNeue
-  static const String fontBebasNeueRegular = 'assets/fonts/Bebas_Neue/BebasNeue-Regular.ttf';
-
-    // # Borel
-  static const String fontBorelRegular = 'assets/fonts/Borel/Borel-Regular.ttf';
+  // Font family names
+  static const String fontFederant = 'Federant';
+  static const String fontBebasNeue = 'BebasNeue';
+  static const String fontBorel = 'Borel';
   
 }
 
