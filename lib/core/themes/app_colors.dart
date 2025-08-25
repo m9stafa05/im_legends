39e4bb857d563757671b0b6a0fe14101fa19c94f
Mustafa Elbaz <m9stafa05@gmail.com>
2025-08-25 15:43:34 +0300

    Refactor AppColors [remove unused colors]

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-
   static const Color darkColor = Color(0xFF191919);
+  static const Color greyColor = Color(0xFFDEDEDE);
   static const Color darkRedColor = Color(0xFF750E21);
-  
-  static const Color orangeColor = Color(0xFFE3651D);
-  static const Color greenColor = Color(0xFFBED754);
-
-  static const Color lightRedColor = Color(0xFFf15034);
 }
