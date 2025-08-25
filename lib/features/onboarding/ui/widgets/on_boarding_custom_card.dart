62ab206d0e9a17dc8ac05a7fb3f77fd78423039c
Mustafa Elbaz <m9stafa05@gmail.com>
2025-08-25 15:51:10 +0300

    Refactor and correct naming to Onboarding Card ui

--- lib/features/onboarding/ui/widgets/on_boarding_custom_card.dart
+++ lib/features/onboarding/ui/widgets/on_boarding_custom_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../../core/themes/app_texts_style.dart';
 import '../../../../core/utils/spacing.dart';
 
@@ -10,31 +9,34 @@ class OnBoardingCustomCard extends StatelessWidget {
     required this.title,
     required this.subTitle,
   });
+
   final String title;
   final String subTitle;
+  static const Color _cardColor = Color(0xFF262A33);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 130.h,
       width: 200.w,
       alignment: Alignment.center,
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20.r),
-        color: Color(0xFF262a33),
+        color: _cardColor,
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
             title,
-            style: AppTextsStyle.font16WhiteBold.copyWith(letterSpacing: -1),
+            style: AppTextStyles.text16WhiteBold.copyWith(letterSpacing: -1),
             textAlign: TextAlign.center,
           ),
           verticalSpacing(5),
           Text(
             subTitle,
-            style: AppTextsStyle.font12GreyRegular,
+            style: AppTextStyles.text12GreyRegular,
             textAlign: TextAlign.center,
           ),
         ],
