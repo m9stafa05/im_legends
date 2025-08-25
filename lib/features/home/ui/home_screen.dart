585d6b6459c5b872ab0b7e84b6b8ae28ebbcbb0d
Mustafa Elbaz <m9stafa05@gmail.com>
2025-08-25 15:59:51 +0300

    Formate spacing

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:im_legends/core/themes/app_texts_style.dart';
+import '../../../core/themes/app_texts_style.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Home Screen', style: AppTextsStyle.font20WhiteBold,)));
+    return Scaffold(
+      body: Center(
+        child: Text('Home Screen', style: AppTextStyles.text18WhiteBold),
+      ),
+    );
   }
 }
