46fa180fe2ee888a50f4f9ca3c4f6481ae4f0d05
Mustafa Elbaz <m9stafa05@gmail.com>
2025-08-25 15:45:01 +0300

    Refactor AppTextStyles by improve naming And sort the styles And Add new text styles

--- lib/core/themes/app_texts_style.dart
+++ lib/core/themes/app_texts_style.dart
@@ -3,57 +3,108 @@ import 'package:im_legends/core/themes/app_colors.dart';
 import 'package:im_legends/core/themes/app_font_weight.dart';
 import 'package:im_legends/core/utils/app_assets.dart';
 
-class AppTextsStyle {
-  // font 20
-  static const font20WhiteBold = TextStyle(
+class AppTextStyles {
+  // ========== FONT SIZE 20 ==========
+  static const text20WhiteBold = TextStyle(
     fontSize: 20,
     fontWeight: FontWeightHelper.bold,
-    fontFamily: AppAssets.fontFederantRegular,
+    fontFamily: AppAssets.fontFederant,
     color: Colors.white,
   );
-  static const font16WhiteBold = TextStyle(
+
+  static const text20GreyBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeightHelper.bold,
+    fontFamily: AppAssets.fontFederant,
+    color: Colors.grey,
+  );
+
+  // ========== FONT SIZE 18 ==========
+  static const text18RedBold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeightHelper.bold,
+    fontFamily: AppAssets.fontBorel,
+    color: AppColors.darkRedColor,
+  );
+
+  static const text18WhiteBold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeightHelper.bold,
+    fontFamily: AppAssets.fontBorel,
+    color: Colors.white,
+  );
+
+  static const text18GreyRegular = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeightHelper.regular,
+    fontFamily: AppAssets.fontBorel,
+    color: Colors.grey,
+  );
+
+  // ========== FONT SIZE 16 ==========
+  static const text16WhiteBold = TextStyle(
     fontSize: 16,
     fontWeight: FontWeightHelper.bold,
-    fontFamily: AppAssets.fontFederantRegular,
+    fontFamily: AppAssets.fontFederant,
     color: Colors.white,
     letterSpacing: .2,
   );
 
-  static const font18RedBold = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeightHelper.bold,
-    fontFamily: AppAssets.fontBorelRegular,
-    color: AppColors.darkRedColor,
-  );
-  static const font18WhiteBold = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeightHelper.bold,
-    fontFamily: AppAssets.fontBorelRegular,
-    color: Colors.white,
-  );
-  static const font12WhiteBold = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeightHelper.bold,
-    fontFamily: AppAssets.fontBebasNeueRegular,
-    color: Colors.white,
-  );
-  static const font14WhiteBold = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeightHelper.bold,
-    fontFamily: AppAssets.fontBebasNeueRegular,
+  static const text16WhiteSemiBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeightHelper.semiBold,
+    fontFamily: AppAssets.fontFederant,
     color: Colors.white,
   );
 
-  static const font14GreyBold = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeightHelper.bold,
-    fontFamily: AppAssets.fontBebasNeueRegular,
+  static const text16GreyRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeightHelper.regular,
+    fontFamily: AppAssets.fontFederant,
     color: Colors.grey,
   );
-  static const font12GreyRegular = TextStyle(
+
+  // ========== FONT SIZE 14 ==========
+  static const text14WhiteBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeightHelper.bold,
+    fontFamily: AppAssets.fontBebasNeue,
+    color: Colors.white,
+  );
+
+  static const text14GreyBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeightHelper.bold,
+    fontFamily: AppAssets.fontBebasNeue,
+    color: Colors.grey,
+  );
+
+  static const text14GreyRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeightHelper.regular,
+    fontFamily: AppAssets.fontBebasNeue,
+    color: Colors.grey,
+  );
+
+  // ========== FONT SIZE 12 ==========
+  static const text12WhiteBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeightHelper.bold,
+    fontFamily: AppAssets.fontBebasNeue,
+    color: Colors.white,
+  );
+
+  static const text12GreyRegular = TextStyle(
     fontSize: 12,
     fontWeight: FontWeightHelper.regular,
-    fontFamily: AppAssets.fontFederantRegular,
+    fontFamily: AppAssets.fontFederant,
     color: Colors.grey,
+  );
+
+  static const text12ErrorRed = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeightHelper.bold,
+    fontFamily: AppAssets.fontFederant,
+    color: AppColors.darkRedColor,
   );
 }
