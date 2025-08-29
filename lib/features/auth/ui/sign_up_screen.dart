98147ee7fdde91859d26d3e33f2678ee332f966a
Mustafa Elbaz <m9stafa05@gmail.com>
2025-08-29 19:40:14 +0300

    Update Sign Up screen Logic

--- lib/features/auth/ui/sign_up_screen.dart
+++ lib/features/auth/ui/sign_up_screen.dart
@@ -30,14 +30,23 @@ class SignUpScreen extends StatelessWidget {
                 verticalSpacing(50),
                 BlocConsumer<AuthCubit, AuthState>(
                   listener: (context, state) {
+                    if (state is AuthLoading) {
+                      _showLoadingDialog(context);
+                    } else {
+                      Navigator.pop(context); // Close loading dialog if open
+                    }
+
                     if (state is AuthSuccess) {
-                      Navigator.pop(context);
+                      final user = state.authResponse.user;
+                      final token = state.authResponse.session?.accessToken;
+
+                      // Optionally store token/user for later use
+                      debugPrint('Logged in user: ${user?.id}');
+                      debugPrint('Access Token: $token');
+
                       context.pushReplacementNamed(Routes.homeScreen);
                     } else if (state is AuthFailure) {
-                      Navigator.pop(context);
                       _showErrorDialog(context, state.errorMessage);
-                    } else if (state is AuthLoading) {
-                      _showLoadingDialog(context);
                     }
                   },
                   builder: (context, state) {
@@ -66,11 +75,13 @@ class SignUpScreen extends StatelessWidget {
   }
 
   void _showErrorDialog(BuildContext context, String message) {
+    debugPrint('Message: $message');
     showDialog(
       context: context,
       builder: (_) => AlertDialog(
         title: const Text('Sign Up Failed'),
         content: Text(message),
+
         actions: [
           TextButton(
             onPressed: () => Navigator.pop(context),
