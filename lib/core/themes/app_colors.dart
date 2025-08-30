6102041ca7f99b9d59cc3aea46fca66f9da28fdb
Mustafa Elbaz <m9stafa05@gmail.com>
2025-08-30 17:41:48 +0300

    Add New color

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color darkColor = Color(0xFF191919);
+  static const Color lightDarkColor = Color(0xFF2A2D3A);
   static const Color greyColor = Color(0xFFDEDEDE);
   static const Color darkRedColor = Color(0xFF750E21);
 }
