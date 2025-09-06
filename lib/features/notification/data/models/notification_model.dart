9e6e72fd7dc4a93dfb376322bc0053469917e72e
Mustafa Elbaz <m9stafa05@gmail.com>
2025-09-07 00:10:55 +0300

    Add Copy with to the model

--- lib/features/notification/data/models/notification_model.dart
+++ lib/features/notification/data/models/notification_model.dart
@@ -43,4 +43,22 @@ class NotificationModel {
   String encode() => json.encode(toJson());
   static NotificationModel decode(String encoded) =>
       NotificationModel.fromJson(json.decode(encoded) as Map<String, dynamic>);
+
+  NotificationModel copyWith({
+    String? id,
+    String? title,
+    String? message,
+    DateTime? time,
+    bool? isRead,
+    NotificationType? type,
+  }) {
+    return NotificationModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      message: message ?? this.message,
+      time: time ?? this.time,
+      isRead: isRead ?? this.isRead,
+      type: type ?? this.type,
+    );
+  }
 }
