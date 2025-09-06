1f806374ba0f067e9977114f327bda8d1ce2fda5
Mustafa Elbaz <m9stafa05@gmail.com>
2025-09-06 17:47:43 +0300

    Refactor for Initializations for Firebase , FirebaseNotifications, and LocalNotificationService

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:im_legends/core/di/dependency_injection.dart';
 import 'package:im_legends/core/router/app_router.dart';
-import 'package:im_legends/features/notification/data/service/firebase_notifications.dart';
+import 'package:im_legends/core/utils/shared_prefs.dart';
+import 'features/notification/data/service/firebase_notifications.dart';
+import 'features/notification/data/service/local_notifications.dart';
 import 'package:im_legends/firebase_options.dart';
 import 'package:im_legends/im_legends_app.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -11,11 +14,14 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 const supabaseUrl = 'https://flutiryhpfdlpizyxqix.supabase.co';
 const supabaseAnonKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsdXRpcnlocGZkbHBpenl4cWl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNTQ3NzIsImV4cCI6MjA3MTczMDc3Mn0.UhojXOtOrnvbwDKvyBVZn3Cl1gdUkr-NYuGBLQXIRi0";
-
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 Future<void> _initServices() async {
   // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  FirebaseNotifications().initialize();
+
+  await LocalNotificationService().initialize();
+  await FirebaseNotifications().initialize();
 
   // Initialize Supabase
   await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
@@ -27,6 +33,7 @@ Future<void> _initServices() async {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await SharedPrefStorage.instance.init();
 
   runApp(
     FutureBuilder(
