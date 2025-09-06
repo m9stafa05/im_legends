ff9000b39a71c13b9cc965e0622b747caca00c79
Mustafa Elbaz <m9stafa05@gmail.com>
2025-09-06 17:46:01 +0300

    Update Notifications Model

--- lib/features/notification/data/models/notification_model.dart
+++ lib/features/notification/data/models/notification_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 enum NotificationType { welcome, update, security, promotion, system }
 
 class NotificationModel {
@@ -16,4 +18,29 @@ class NotificationModel {
     required this.isRead,
     required this.type,
   });
+
+  factory NotificationModel.fromJson(Map<String, dynamic> json) {
+    return NotificationModel(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      message: json['message'] as String,
+      time: DateTime.parse(json['time'] as String),
+      isRead: json['isRead'] as bool,
+      type: NotificationType.values[json['type'] as int],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'message': message,
+    'time': time.toIso8601String(),
+    'isRead': isRead,
+    'type': type.index,
+  };
+
+  /// convenience to/from encoded JSON string
+  String encode() => json.encode(toJson());
+  static NotificationModel decode(String encoded) =>
+      NotificationModel.fromJson(json.decode(encoded) as Map<String, dynamic>);
 }
