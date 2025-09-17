8ccf4c34c6aa66ccbb9602d56ca7dc46bfe21759
Mustafa Elbaz <m9stafa05@gmail.com>
2025-09-17 03:17:06 +0300

    Remove Unnessery Init

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/di/dependency_injection.dart';
 import 'core/router/app_router.dart';
@@ -12,13 +12,8 @@ import 'features/notification/data/service/local_notifications.dart';
 import 'features/notification/data/service/firebase_notifications_service.dart';
 import 'firebase_options.dart';
 import 'im_legends_app.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
-// Global notifications plugin
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-/// Initialize core services
+/// === Initialize core services ===
 Future<void> _initServices() async {
   // Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -36,41 +31,27 @@ Future<void> _initServices() async {
   setupGetIt();
 }
 
-/// Main entry point
+/// === Main entry point ===
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Load env vars
   await dotenv.load(fileName: ".env");
 
-  // ScreenUtil
+  // Ensure screen util sizing
   await ScreenUtil.ensureScreenSize();
 
-  // SharedPrefs
+  // Shared Preferences
   await SharedPrefStorage.instance.init();
 
   // Register background handler BEFORE runApp
   FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
 
-  // Run app after async init
-  runApp(
-    FutureBuilder(
-      future: _initServices(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return IMLegendsApp(router: router);
-        }
-        // Splash/loading screen
-        return const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: Scaffold(
-            backgroundColor: Colors.black,
-            body: Center(child: CircularProgressIndicator(color: Colors.white)),
-          ),
-        );
-      },
-    ),
-  );
+  // Init services
+  await _initServices();
+
+  // Run app
+  runApp(IMLegendsApp(router: router));
 }
 
 // Run with:
