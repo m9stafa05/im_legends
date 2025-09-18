4139b02bf1dc8f93b6ea07ee3f48628e2a1a021f
Mustafa Elbaz <m9stafa05@gmail.com>
2025-09-18 04:47:38 +0300

    Build LeaderBoardCubit And States For mange LeaderBoard Logic

---
+++ lib/features/home/logic/cubit/leader_board_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import '../../data/models/players_states_model.dart';
+import '../../data/repo/leader_board_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'leader_board_state.dart';
+
+class LeaderBoardCubit extends Cubit<LeaderBoardState> {
+  final LeaderBoardRepo repo;
+  LeaderBoardCubit({required this.repo}) : super(LeaderBoardInitial());
+
+  Future<void> loadLeaderboard() async {
+    emit(LeaderBoardLoading());
+    try {
+      final leaderboard = await repo.calculateLeaderboard();
+      emit(LeaderBoardSuccess(leaderboard));
+    } catch (e) {
+      emit(LeaderBoardFailure(e.toString()));
+    }
+  }
+}
