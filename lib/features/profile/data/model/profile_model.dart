75424815b3ba6bb6c8036eee34dec3ab46ab9448
Mustafa Elbaz <m9stafa05@gmail.com>
2025-09-23 18:09:35 +0300

    Add ProfileRepo to manage user profile data and logout functionality

---
+++ lib/features/profile/data/model/profile_model.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:im_legends/core/models/players_states_model.dart';
+import 'package:im_legends/core/models/user_data.dart';
+
+class UserProfileModel {
+  final UserData user;
+  final PlayerStatsModel stats;
+
+  UserProfileModel({required this.user, required this.stats});
+}
---
+++ lib/features/profile/data/repo/profile_repo.dart
@@ -0,0 +1,36 @@
+import 'package:im_legends/core/utils/secure_storage.dart';
+import 'package:im_legends/features/profile/data/model/profile_model.dart';
+
+import '../service/profile_service.dart';
+
+class ProfileRepo {
+  final ProfileService profileService;
+  final SecureStorage secureStorage = SecureStorage();
+
+  ProfileRepo({required this.profileService});
+
+  /// Get full profile data + stats
+  Future<UserProfileModel?> getProfileWithStats() async {
+    final userId = await secureStorage.getUserId();
+
+    if (userId == null || userId.isEmpty) {
+      throw Exception("User ID not found in secure storage");
+    }
+
+    try {
+      final user = await profileService.fetchUserData(userId);
+      final stats = await profileService.fetchPlayerStats(userId);
+
+      return UserProfileModel(user: user, stats: stats);
+    } catch (e) {
+      print("❌ Error fetching profile with stats: $e");
+      return null;
+    }
+  }
+
+  /// Logout user: clears secure storage + Supabase session
+  Future<void> logout() async {
+    await secureStorage.clearToken();
+    await profileService.logout();
+  }
+}
