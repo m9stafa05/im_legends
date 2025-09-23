ac5a5f31b9a00ff9ea61d55619b8a4132dc20ca6
Mustafa Elbaz <m9stafa05@gmail.com>
2025-09-23 16:30:57 +0300

    Build match history cubit and states for history logic

---
+++ lib/features/history/logic/cubit/match_history_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import '../../data/models/match_history_card_model.dart';
+import '../../data/repo/history_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'match_history_state.dart';
+
+class MatchHistoryCubit extends Cubit<MatchHistoryState> {
+  MatchHistoryCubit() : super(MatchHistoryInitial());
+
+  void getMatchHistory() async {
+    emit(MatchHistoryLoading());
+    try {
+      final matches = await HistoryRepo().fetchMatches();
+      emit(MatchHistorySuccess(matches: matches));
+    } catch (e) {
+      emit(MatchHistoryError(errorMessage: e.toString()));
+    }
+  }
+}
