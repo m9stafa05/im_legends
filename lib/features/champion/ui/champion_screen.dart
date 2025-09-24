a95a87a35a4c408a356f6d24ab5f6c51c0df3f4c
Mustafa Elbaz <m9stafa05@gmail.com>
2025-09-24 22:19:16 +0300

    Refactor ChampionScreen to use Bloc for state management and update ChampionTopThree and ChampionsCard to utilize ChampionPlayerModel

--- lib/features/champion/ui/champion_screen.dart
+++ lib/features/champion/ui/champion_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../logic/cubit/champion_cubit.dart';
 import '../../../core/widgets/custom_app_bar.dart';
 import 'widgets/champion_header.dart';
 import 'widgets/champion_states.dart';
@@ -9,18 +11,36 @@ class ChampionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          CustomAppBar(title: 'Champions'),
+          const CustomAppBar(title: 'Champions'),
           Expanded(
             child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  ChampionHeader(name: 'Mustafa Elbaz'),
-                  ChampionStats(goals: 10, points: 50, wins: 5, matches: 7),
-                  ChampionTopThree(),
-                ],
+              child: BlocBuilder<ChampionCubit, ChampionState>(
+                builder: (context, state) {
+                  if (state is ChampionLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is ChampionSuccess) {
+                    final champion = state.players[0];
+                    final topThree = state.players;
+                    return Column(
+                      children: [
+                        ChampionHeader(name: champion.user.name),
+                        ChampionStats(
+                          goals: champion.stats.goals,
+                          points: champion.stats.points,
+                          wins: champion.stats.wins,
+                          matches: champion.stats.matchesPlayed,
+                        ),
+                        ChampionTopThree(topThree: topThree),
+                      ],
+                    );
+                  } else if (state is ChampionFailure) {
+                    return Center(child: Text(state.errorMessage));
+                  }
+                  return Container();
+                },
               ),
             ),
           ),
