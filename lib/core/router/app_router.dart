43e3daa0a39cbd79bce46055598a4ad882262674
Mustafa Elbaz <m9stafa05@gmail.com>
2025-10-02 13:51:30 +0300

    Refactor app router: streamline imports, enhance route definitions, and improve provider setup and fix logic error

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,46 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:im_legends/features/onboarding/ui/on_boarding_screen.dart';
 
-import '../di/dependency_injection.dart';
-import '../widgets/not_screen_found.dart';
-import '../widgets/main_scaffold.dart';
 import 'route_paths.dart';
+import '../di/dependency_injection.dart';
+import '../widgets/main_scaffold.dart';
+import '../widgets/not_screen_found.dart';
 
-// Feature imports (UI + Cubits)
+import '../../features/onboarding/ui/on_boarding_screen.dart';
 import '../../features/auth/ui/login_screen.dart';
 import '../../features/auth/ui/sign_up_screen.dart';
-
-import '../../features/auth/logic/cubit/auth_cubit.dart';
-import '../../features/auth/data/repo/auth_repo.dart';
-
 import '../../features/home/ui/home_screen.dart';
-import '../../features/home/logic/cubit/leader_board_cubit.dart';
-import '../../features/home/data/repo/leader_board_repo.dart';
-
 import '../../features/champion/ui/champion_screen.dart';
-import '../../features/champion/logic/cubit/champion_cubit.dart';
-import '../../features/champion/data/repo/champion_repo.dart';
-
 import '../../features/history/ui/history_screen.dart';
-import '../../features/history/logic/cubit/match_history_cubit.dart';
-
 import '../../features/profile/ui/profile_screen.dart';
-import '../../features/profile/logic/cubit/profile_cubit.dart';
-import '../../features/profile/data/repo/profile_repo.dart';
-
 import '../../features/add_match/ui/add_match_screen.dart';
-import '../../features/add_match/logic/cubit/add_match_cubit.dart';
-import '../../features/add_match/data/repo/add_match_repo.dart';
-
 import '../../features/notification/ui/notifications_screen.dart';
 import '../../features/notification/ui/widgets/notification_details_screen.dart';
-import '../../features/notification/logic/cubit/notifications_cubit.dart';
-import '../../features/notification/data/repos/notification_repo.dart';
-import '../../features/notification/data/models/notification_model.dart';
 
+import '../../features/auth/logic/cubit/auth_cubit.dart';
 import '../../features/auth/data/service/auth_service.dart';
+import '../../features/home/logic/cubit/leader_board_cubit.dart';
+import '../../features/champion/logic/cubit/champion_cubit.dart';
+import '../../features/profile/logic/cubit/profile_cubit.dart';
+import '../../features/add_match/logic/cubit/add_match_cubit.dart';
+import '../../features/notification/logic/cubit/notifications_cubit.dart';
+import '../../features/notification/data/models/notification_model.dart';
+import '../../features/history/logic/cubit/match_history_cubit.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -62,17 +48,6 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: Routes.signUpScreen,
       builder: (_, __) => _withAuthProviders(const SignUpScreen()),
-    ),
-
-    // Notifications
-    GoRoute(
-      path: Routes.notificationsScreen,
-      builder: (_, __) => BlocProvider(
-        create: (_) =>
-            NotificationsCubit(notificationRepo: getIt<NotificationRepo>())
-              ..fetchNotifications(),
-        child: const NotificationsScreen(),
-      ),
     ),
     GoRoute(
       path: Routes.notificationDetailsScreen,
@@ -106,6 +81,10 @@ final GoRouter router = GoRouter(
           path: Routes.profileScreen,
           builder: (_, __) => const ProfileScreen(),
         ),
+        GoRoute(
+          path: Routes.notificationsScreen,
+          builder: (_, __) => const NotificationsScreen(),
+        ),
       ],
     ),
   ],
@@ -118,11 +97,8 @@ final GoRouter router = GoRouter(
 Widget _withAuthProviders(Widget child) {
   return MultiBlocProvider(
     providers: [
-      BlocProvider(create: (_) => AuthCubit(authRepo: getIt<AuthRepo>())),
-      BlocProvider(
-        create: (_) =>
-            NotificationsCubit(notificationRepo: getIt<NotificationRepo>()),
-      ),
+      BlocProvider(create: (_) => getIt<AuthCubit>()),
+      BlocProvider(create: (_) => getIt<NotificationsCubit>()),
     ],
     child: child,
   );
@@ -131,26 +107,13 @@ Widget _withAuthProviders(Widget child) {
 Widget _withMainProviders(Widget child) {
   return MultiBlocProvider(
     providers: [
-      BlocProvider(
-        create: (_) =>
-            LeaderBoardCubit(repo: getIt<LeaderBoardRepo>())..loadLeaderboard(),
-      ),
+      BlocProvider(create: (_) => getIt<LeaderBoardCubit>()..loadLeaderboard()),
       BlocProvider(create: (_) => MatchHistoryCubit()..getMatchHistory()),
+      BlocProvider(create: (_) => getIt<AddMatchCubit>()),
+      BlocProvider(create: (_) => getIt<ProfileCubit>()..fetchProfile()),
+      BlocProvider(create: (_) => getIt<ChampionCubit>()..fetchTopThree()),
       BlocProvider(
-        create: (_) => AddMatchCubit(addMatchRepo: getIt<AddMatchRepo>()),
-      ),
-      BlocProvider(
-        create: (_) =>
-            ProfileCubit(profileRepo: getIt<ProfileRepo>())..fetchProfile(),
-      ),
-      BlocProvider(
-        create: (_) =>
-            ChampionCubit(repository: getIt<ChampionRepo>())..fetchTopThree(),
-      ),
-      BlocProvider(
-        create: (_) =>
-            NotificationsCubit(notificationRepo: getIt<NotificationRepo>())
-              ..fetchNotifications(),
+        create: (_) => getIt<NotificationsCubit>()..fetchNotifications(),
       ),
     ],
     child: child,
